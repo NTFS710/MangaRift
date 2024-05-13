29afc437a131481aa9a4166a003793516d4eeba1
Nathan Ferreira Silva <nathanfs710@gmail.com>
2024-05-13 20:50:30 -0300

    uploading reading modes

--- app/src/main/java/com/sephirita/mangarift/ui/components/reader/ReaderChapter.kt
+++ app/src/main/java/com/sephirita/mangarift/ui/components/reader/ReaderChapter.kt
@@ -5,21 +5,32 @@ import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.gestures.rememberTransformableState
 import androidx.compose.foundation.gestures.transformable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.VerticalPager
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
@@ -32,15 +43,18 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
 import coil.compose.AsyncImage
+import com.sephirita.mangarift.R
 import org.koin.androidx.compose.koinViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -54,6 +68,7 @@ fun ReaderChapter(chapterId: String) {
     var scale by remember { mutableFloatStateOf(1f) }
     var offset by remember { mutableStateOf(Offset.Zero) }
     var showDialog by remember { mutableStateOf(false) }
+    var horizontalReading by remember { mutableStateOf(true) }
     val pagerState = rememberPagerState(pageCount = { chapterPages.size })
 
     LaunchedEffect(key1 = Unit) {
@@ -103,26 +118,67 @@ fun ReaderChapter(chapterId: String) {
 
         if (showDialog) {
             Dialog(onDismissRequest = { showDialog = false }) {
-                Box(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                ) {
+                Box {
                     Column {
                         Button(
-                            modifier = Modifier.fillMaxWidth(),
-                            onClick = { /*TODO*/ }
+                            modifier = Modifier
+                                .padding(horizontal = 4.dp)
+                                .width(120.dp)
+                                .align(Alignment.Start),
+                            shape = RoundedCornerShape(8.dp),
+                            contentPadding = PaddingValues(start = 8.dp),
+                            onClick = {
+                                horizontalReading = true
+                                showDialog = false
+                            }
                         ) {
-                            Row {
-//                                Icon(painter = , contentDescription = )
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Start,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    modifier = Modifier
+                                        .size(24.dp),
+                                    painter = painterResource(id = R.drawable.ic_read_type_horizontal),
+                                    contentDescription = "Horizontal Reader Type"
+                                )
+                                VerticalDivider(
+                                    modifier = Modifier
+                                        .height(24.dp)
+                                        .padding(horizontal = 8.dp),
+                                    color = Color.White
+                                )
                                 Text(text = "Padrão")
                             }
                         }
                         Button(
-                            modifier = Modifier.fillMaxWidth(),
-                            onClick = { /*TODO*/ }
+                            modifier = Modifier
+                                .padding(horizontal = 4.dp)
+                                .width(120.dp),
+                            shape = RoundedCornerShape(8.dp),
+                            contentPadding = PaddingValues(horizontal = 8.dp),
+                            onClick = {
+                                horizontalReading = false
+                                showDialog = false
+                            }
                         ) {
-                            Row {
-//                                Icon(painter = , contentDescription = )
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Start,
+                                verticalAlignment = Alignment.CenterVertically
+                            ) {
+                                Icon(
+                                    modifier = Modifier.size(24.dp),
+                                    painter = painterResource(id = R.drawable.ic_read_type_vertical),
+                                    contentDescription = "Vertical Reader Type"
+                                )
+                                VerticalDivider(
+                                    modifier = Modifier
+                                        .height(24.dp)
+                                        .padding(horizontal = 8.dp),
+                                    color = Color.White
+                                )
                                 Text(text = "Webtoon")
                             }
                         }
@@ -131,28 +187,55 @@ fun ReaderChapter(chapterId: String) {
             }
         }
 
-        HorizontalPager(
-            state = pagerState,
-            modifier = Modifier
-                .fillMaxSize()
-                .graphicsLayer(
-                    scaleX = scale,
-                    scaleY = scale,
-                    translationX = offset.x,
-                    translationY = offset.y
-                ),
-            beyondViewportPageCount = 3
-        ) {
-            val currentItem = chapterPages[it]
-            AsyncImage(
+        if (horizontalReading) {
+            HorizontalPager(
+                state = pagerState,
                 modifier = Modifier
                     .fillMaxSize()
-                    .transformable(state = transformableState, canPan = { scale != 1f }),
-                contentScale = ContentScale.Fit,
-                alignment = Alignment.Center,
-                model = currentItem,
-                contentDescription = "PDF Image"
-            )
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        translationX = offset.x,
+                        translationY = offset.y
+                    ),
+                beyondViewportPageCount = 3
+            ) {
+                val currentItem = chapterPages[it]
+                AsyncImage(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .transformable(state = transformableState, canPan = { scale != 1f }),
+                    contentScale = ContentScale.Fit,
+                    alignment = Alignment.Center,
+                    model = currentItem,
+                    contentDescription = "PDF Image"
+                )
+            }
+        } else {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .graphicsLayer(
+                        scaleX = scale,
+                        scaleY = scale,
+                        translationX = offset.x,
+                        translationY = offset.y
+                    )
+                    .transformable(state = transformableState, canPan = { scale != 1f })
+                    .verticalScroll(rememberScrollState()),
+            ) {
+                chapterPages.forEachIndexed { index, it ->
+                    val currentItem = chapterPages[index]
+                    AsyncImage(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        contentScale = ContentScale.Fit,
+                        alignment = Alignment.Center,
+                        model = currentItem,
+                        contentDescription = "PDF Image"
+                    )
+                }
+            }
         }
     }
 }
